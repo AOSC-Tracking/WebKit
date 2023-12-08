4c9425f9126b167b3fb0d7b75e4a7f973e17ab5a
Youenn Fablet <youennf@gmail.com>
2023-12-08 13:33:31 +0100

    Calling configure on WebCodecs Video Encoder triggers a new key frame each time even if the configuration is only changing the bitrate https://bugs.webkit.org/show_bug.cgi?id=261402 rdar://115482127
    Reviewed by NOBODY (OOPS!).
    
    Check the new configuration is the same as the new one, except for bitrate and framerate.
    If that is the case, we call setRate instead of recreating the encoder.
    Implement setRate for VPX and remote encoders.
    
    Covered by newly added test.
    
    * LayoutTests/http/wpt/webcodecs/encode-bitrate-change-expected.txt: Added.
    * LayoutTests/http/wpt/webcodecs/encode-bitrate-change.html: Added.
    * Source/WebCore/Modules/webcodecs/WebCodecsVideoEncoder.cpp:
    (WebCore::WebCodecsVideoEncoder::updateRates):
    (WebCore::WebCodecsVideoEncoder::configure):
    * Source/WebCore/Modules/webcodecs/WebCodecsVideoEncoder.h:
    * Source/WebCore/Modules/webcodecs/WebCodecsVideoEncoderConfig.h:
    (WebCore::isSameConfigurationExceptBitrateAndFramerate):
    * Source/WebCore/platform/VideoEncoder.h:
    * Source/WebCore/platform/libwebrtc/LibWebRTCVPXVideoEncoder.cpp:
    (WebCore::LibWebRTCVPXVideoEncoder::setRates):
    (WebCore::computeAllocation):
    (WebCore::LibWebRTCVPXInternalVideoEncoder::initialize):
    (WebCore::LibWebRTCVPXInternalVideoEncoder::encode):
    (WebCore::LibWebRTCVPXInternalVideoEncoder::setRates):
    * Source/WebCore/platform/libwebrtc/LibWebRTCVPXVideoEncoder.h:
    * Source/WebKit/WebProcess/GPU/media/RemoteVideoCodecFactory.cpp:
    (WebKit::RemoteVideoEncoder::setRates):
    (WebKit::RemoteVideoEncoder::flush):
    * Source/WebKit/WebProcess/GPU/webrtc/LibWebRTCCodecs.cpp:
    (WebKit::setEncodeRatesCallback):
    (WebKit::LibWebRTCCodecs::setEncodeRates):
    * Source/WebKit/WebProcess/GPU/webrtc/LibWebRTCCodecs.h:

--- Source/WebCore/Modules/webcodecs/WebCodecsVideoEncoder.h
+++ Source/WebCore/Modules/webcodecs/WebCodecsVideoEncoder.h
@@ -96,6 +96,7 @@ private:
     void queueControlMessageAndProcess(Function<void()>&&);
     void processControlMessageQueue();
     WebCodecsEncodedVideoChunkMetadata createEncodedChunkMetadata(std::optional<unsigned>);
+    bool updateRates(const WebCodecsVideoEncoderConfig&);
 
     WebCodecsCodecState m_state { WebCodecsCodecState::Unconfigured };
     size_t m_encodeQueueSize { 0 };
