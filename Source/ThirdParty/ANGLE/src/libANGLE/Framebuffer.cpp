2b740c0f69106423ea94ec1b6b8c2670260e0a26
Dan Glastonbury <djg@apple.com>
2024-05-14 14:50:37 +0930

    [WebXR] Implicitly clear WebXROpaqueFramebuffer https://bugs.webkit.org/show_bug.cgi?id=274136 rdar://128041214
    Reviewed by NOBODY (OOPS!).
    
    The performance panel of GPU debugger shows 3.5ms spent just clearing and
    writing render targets at the start of frame due to our explicit glClear to
    match the WebXR spec.
    
    This change makes use of the robust resource feature of ANGLE to initialize
    render target attachments just before their first use. To achieve this,
    framebuffer discard/invalidate now marks attachments as needing initialization
    when robust resource init is enabled is ANGLE.
    
    This change adds framebufferDiscard and IPC messages gated on availability of
    WebXR for WebXROpaqueFramebuffer to use.
    
    * Source/ThirdParty/ANGLE/src/libANGLE/Framebuffer.cpp:
    (gl::Framebuffer::discard):
    (gl::Framebuffer::invalidate):
    (gl::Framebuffer::markDrawAttachmentsNeedInit):
    * Source/ThirdParty/ANGLE/src/libANGLE/Framebuffer.h:
    * Source/ThirdParty/ANGLE/src/libANGLE/renderer/metal/mtl_state_cache.h:
    * Source/WebCore/Modules/webxr/WebXROpaqueFramebufferCocoa.cpp:
    (WebCore::WebXROpaqueFramebuffer::startFrame):
    (WebCore::WebXROpaqueFramebuffer::blitSharedToLayered):
    * Source/WebCore/platform/graphics/GraphicsContextGL.cpp:
    (WebCore::GraphicsContextGL::framebufferDiscard):
    * Source/WebCore/platform/graphics/GraphicsContextGL.h:
    * Source/WebCore/platform/graphics/cocoa/GraphicsContextGLCocoa.h:
    * Source/WebCore/platform/graphics/cocoa/GraphicsContextGLCocoa.mm:
    (WebCore::GraphicsContextGLCocoa::framebufferDiscard):
    (WebCore::GraphicsContextGLCocoa::enableRequiredWebXRExtensionsImpl):
    * Source/WebKit/GPUProcess/graphics/RemoteGraphicsContextGL.messages.in:
    * Source/WebKit/GPUProcess/graphics/RemoteGraphicsContextGLFunctionsGenerated.h:
    (framebufferDiscard):
    * Source/WebKit/WebProcess/GPU/graphics/RemoteGraphicsContextGLProxy.h:
    * Source/WebKit/WebProcess/GPU/graphics/RemoteGraphicsContextGLProxyFunctionsGenerated.cpp:
    (WebKit::RemoteGraphicsContextGLProxy::framebufferDiscard):

--- Source/ThirdParty/ANGLE/src/libANGLE/Framebuffer.cpp
+++ Source/ThirdParty/ANGLE/src/libANGLE/Framebuffer.cpp
@@ -1680,6 +1680,8 @@ angle::Result Framebuffer::discard(const Context *context, size_t count, const G
     // Back-ends might make the contents of the FBO undefined. In WebGL 2.0, invalidate operations
     // can be no-ops, so we should probably do that to ensure consistency.
     // TODO(jmadill): WebGL behaviour, and robust resource init behaviour without WebGL.
+    if (context->isRobustResourceInitEnabled())
+        markDrawAttachmentsNeedInit(count, attachments);
 
     return mImpl->discard(context, count, attachments);
 }
@@ -1691,6 +1693,8 @@ angle::Result Framebuffer::invalidate(const Context *context,
     // Back-ends might make the contents of the FBO undefined. In WebGL 2.0, invalidate operations
     // can be no-ops, so we should probably do that to ensure consistency.
     // TODO(jmadill): WebGL behaviour, and robust resource init behaviour without WebGL.
+    if (context->isRobustResourceInitEnabled())
+        markDrawAttachmentsNeedInit(count, attachments);
 
     return mImpl->invalidate(context, count, attachments);
 }
@@ -2896,4 +2900,39 @@ angle::Result Framebuffer::syncAttachmentState(const Context *context,
 
     return angle::Result::Continue;
 }
+
+void Framebuffer::markDrawAttachmentsNeedInit(size_t count, const GLenum *attachments)
+{
+    for (size_t attachmentIdx = 0; attachmentIdx < count; attachmentIdx++)
+    {
+        GLenum attachmentBindPoint = attachments[attachmentIdx];
+        switch (attachmentBindPoint)
+        {
+        case GL_DEPTH_ATTACHMENT:
+        case GL_DEPTH_EXT:
+            if (mState.mDepthAttachment.isAttached())
+            {
+                mState.mResourceNeedsInit.set(DIRTY_BIT_DEPTH_ATTACHMENT);
+            }
+            break;
+        case GL_STENCIL_ATTACHMENT:
+        case GL_STENCIL_EXT:
+            if (mState.mStencilAttachment.isAttached())
+            {
+                mState.mResourceNeedsInit.set(DIRTY_BIT_STENCIL_ATTACHMENT);
+            }
+            break;
+        default:
+            if (attachmentBindPoint == GL_COLOR_EXT)
+            {
+                attachmentBindPoint = GL_COLOR_ATTACHMENT0;
+            }
+            int colorDirtyBit = attachmentBindPoint - GL_COLOR_ATTACHMENT0;
+            if (colorDirtyBit >= 0 && colorDirtyBit < IMPLEMENTATION_MAX_DRAW_BUFFERS && mState.mColorAttachments[colorDirtyBit].isAttached())
+                mState.mResourceNeedsInit.set(colorDirtyBit);
+            break;
+        }
+    }
+}
+
 }  // namespace gl
--- Source/ThirdParty/ANGLE/src/libANGLE/Framebuffer.h
+++ Source/ThirdParty/ANGLE/src/libANGLE/Framebuffer.h
@@ -549,6 +549,8 @@ class Framebuffer final : public angle::ObserverInterface,
                                       Command command,
                                       const FramebufferAttachment *attachment) const;
 
+    void markDrawAttachmentsNeedInit(size_t count, const GLenum *attachments);
+
     FramebufferState mState;
     rx::FramebufferImpl *mImpl;
 
