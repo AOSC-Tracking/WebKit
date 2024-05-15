a1980c75c797a0728a2c961b94a2d1f9e2bcf975
Dan Glastonbury <djg@apple.com>
2024-05-15 13:50:56 +0930

    [WebGL] Log KHR_debug messages to system console https://bugs.webkit.org/show_bug.cgi?id=274197 rdar://128107146
    Reviewed by NOBODY (OOPS!).
    
    To aid debugging WebGL apps, surface debug messages generated by the KHR_debug
    extension for the GL_DEBUG_SOURCE_API message source to the system
    console. Errors are formatted in the same manner as other errors resulting from
    WebGL operations, where as all other message types are formatted as informational.
    
    * LayoutTests/fast/canvas/webgl/debug-messages-to-console-expected.txt: Added.
    * LayoutTests/fast/canvas/webgl/debug-messages-to-console.html: Added.
    * LayoutTests/fast/canvas/webgl/index-validation-with-subsequent-draws.html:
    * LayoutTests/fast/canvas/webgl/invalid-UTF-16.html:
    * LayoutTests/fast/canvas/webgl/largeBuffer.html:
    * LayoutTests/fast/canvas/webgl/webgl2-texStorage.html:
    * LayoutTests/webgl/webgl-allow-shared.html:
    * LayoutTests/webgl/webgl-multi-draw-noop.html:
    * Source/WebCore/html/canvas/WebGLRenderingContextBase.cpp:
    (WebCore::glEnumToErrorCode):
    (WebCore::resolveGraphicsContextGLAttributes):
    (WebCore::debugMessageTypeToString):
    (WebCore::debugMessageSeverityToString):
    (WebCore::WebGLRenderingContextBase::addDebugMessage):
    * Source/WebCore/html/canvas/WebGLRenderingContextBase.h:
    * Source/WebCore/platform/graphics/GraphicsContextGL.h:
    * Source/WebCore/platform/graphics/angle/GraphicsContextGLANGLE.cpp:
    (WebCore::GraphicsContextGLANGLE::initialize):
    * Source/WebCore/platform/graphics/cocoa/GraphicsContextGLCocoa.mm:
    (WebCore::GraphicsContextGLANGLE::~GraphicsContextGLANGLE):
    * Source/WebCore/platform/graphics/texmap/GraphicsContextGLTextureMapperANGLE.cpp:
    (WebCore::GraphicsContextGLANGLE::~GraphicsContextGLANGLE):
    * Source/WebKit/GPUProcess/graphics/RemoteGraphicsContextGL.cpp:
    (WebKit::RemoteGraphicsContextGL::addDebugMessage):
    * Source/WebKit/GPUProcess/graphics/RemoteGraphicsContextGL.h:
    * Source/WebKit/WebProcess/GPU/graphics/RemoteGraphicsContextGLProxy.cpp:
    (WebKit::RemoteGraphicsContextGLProxy::wasLost):
    (WebKit::RemoteGraphicsContextGLProxy::addDebugMessage):
    * Source/WebKit/WebProcess/GPU/graphics/RemoteGraphicsContextGLProxy.h:
    * Source/WebKit/WebProcess/GPU/graphics/RemoteGraphicsContextGLProxy.messages.in:
    * Tools/TestWebKitAPI/Tests/WebCore/cocoa/TestGraphicsContextGLCocoa.mm:

--- Source/WebKit/WebProcess/GPU/graphics/RemoteGraphicsContextGLProxy.cpp
+++ Source/WebKit/WebProcess/GPU/graphics/RemoteGraphicsContextGLProxy.cpp
@@ -439,7 +439,14 @@ void RemoteGraphicsContextGLProxy::wasLost()
     if (isContextLost())
         return;
     markContextLost();
+}
 
+void RemoteGraphicsContextGLProxy::addDebugMessage(GCGLenum type, GCGLenum id, GCGLenum severity, String&& message)
+{
+    if (isContextLost())
+        return;
+    if (m_client)
+        m_client->addDebugMessage(type, id, severity, WTFMove(message));
 }
 
 void RemoteGraphicsContextGLProxy::markContextLost()
