172c27bd44b37de599d7061e79b2755640b67ae4
Per Arne Vollan <pvollan@apple.com>
2024-05-23 07:42:56 -0700

    Invalidate process object in XPC connection termination watchdog https://bugs.webkit.org/show_bug.cgi?id=273773 rdar://126373957
    Reviewed by NOBODY (OOPS!).
    
    Invalidate process object instead of calling xpc_connection_kill. This will send a termination request
    which will terminate the process. This termination approach is only available for WebKit process
    extensions.
    
    * Source/WebKit/Platform/IPC/Connection.h:
    (IPC::Connection::Client::didRequestProcessTermination):
    * Source/WebKit/Platform/IPC/cocoa/ConnectionCocoa.mm:
    (IPC::Connection::kill):
    * Source/WebKit/Platform/cocoa/XPCUtilities.h:
    * Source/WebKit/Platform/cocoa/XPCUtilities.mm:
    * Source/WebKit/UIProcess/AuxiliaryProcessProxy.cpp:
    (WebKit::AuxiliaryProcessProxy::terminate):
    * Source/WebKit/UIProcess/AuxiliaryProcessProxy.h:
    * Source/WebKit/UIProcess/AuxiliaryProcessProxy.cpp:
    (WebKit::AuxiliaryProcessProxy::requestRemoteProcessTermination):
    * Source/WebKit/UIProcess/Cocoa/XPCConnectionTerminationWatchdog.h:
    * Source/WebKit/UIProcess/Cocoa/XPCConnectionTerminationWatchdog.mm:
    (WebKit::XPCConnectionTerminationWatchdog::watchdogTimerFired):
    * Source/WebKit/UIProcess/Launcher/cocoa/ProcessLauncherCocoa.mm:
    (WebKit::ProcessLauncher::terminateProcess):
    (WebKit::ProcessLauncher::terminateXPCConnection):

--- Source/WebKit/Platform/IPC/cocoa/ConnectionCocoa.mm
+++ Source/WebKit/Platform/IPC/cocoa/ConnectionCocoa.mm
@@ -573,6 +573,7 @@ std::optional<audit_token_t> Connection::getAuditToken()
     return WTFMove(auditToken);
 }
 
+#if !USE(EXTENSIONKIT)
 bool Connection::kill()
 {
     if (m_xpcConnection) {
@@ -580,9 +581,9 @@ bool Connection::kill()
         m_wasKilled = true;
         return true;
     }
-
     return false;
 }
+#endif
 
 pid_t Connection::remoteProcessID() const
 {
