02af82c6f2720e2c439fbdf41a1ecbc95853e5d1
Matt Woodrow <mattwoodrow@apple.com>
2024-05-24 14:51:35 +1200

    Toggling visibility:hidden on the parent of a <canvas> can cause the previous frame's contents to be shown. https://bugs.webkit.org/show_bug.cgi?id=274262 <rdar://128226178>
    Reviewed by NOBODY (OOPS!).
    
    We should only try to use 'copy' compositing mode to transfer the canvas buffer into the layer (and thus
    omit any pixel clearing) if there is visible content to copy.
    
    * LayoutTests/compositing/canvas/copy-backing-store-visibility-hidden-expected.html: Added.
    * LayoutTests/compositing/canvas/copy-backing-store-visibility-hidden.html: Added.
    * Source/WebCore/rendering/RenderLayerBacking.cpp:
    (WebCore::RenderLayerBacking::updateConfiguration):

--- Source/WebCore/rendering/RenderLayerBacking.cpp
+++ Source/WebCore/rendering/RenderLayerBacking.cpp
@@ -1151,7 +1151,7 @@ bool RenderLayerBacking::updateConfiguration(const RenderLayer* compositingAnces
         layerConfigChanged = true;
     }
 
-    bool shouldPaintUsingCompositeCopy = unscaledBitmap && is<RenderHTMLCanvas>(renderer());
+    bool shouldPaintUsingCompositeCopy = unscaledBitmap && is<RenderHTMLCanvas>(renderer()) && m_owningLayer.hasVisibleContent();
     if (shouldPaintUsingCompositeCopy != m_shouldPaintUsingCompositeCopy) {
         m_shouldPaintUsingCompositeCopy = shouldPaintUsingCompositeCopy;
         m_graphicsLayer->setShouldPaintUsingCompositeCopy(shouldPaintUsingCompositeCopy);
